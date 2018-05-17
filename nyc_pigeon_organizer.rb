312edae5492c908266912101dd0ac1cf4c582293
DevinPierce <devinmpierce@gmail.com>
2018-05-17 19:43:24 -0400

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute, attribute_hash|
+    attribute_hash.each do |aspect, names|
+      names.each do |name|
+        if !new_hash.key?(name)
+          new_hash[name] = {}
+        end
+        if !new_hash[name][attribute].is_a?(Array)
+          new_hash[name][attribute] = []
+          new_hash[name][attribute] << aspect.to_s
+        else
+          new_hash[name][attribute] << aspect.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
