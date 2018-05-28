41f4c0cf97b7a5961143ef285d58e18da84599de
github username <github email address>
2018-05-27 18:00:18 -0700

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+# Iterates over the pigeon hash, collecting each pigeon by name and inserting it
+# as the key of a new hash where each name holds the attributes for that bird.
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, category|
+    category.each do |descriptor, name_array|
+      name_array.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+          if !pigeon_list[name][attribute]
+            pigeon_list[name][attribute] = []
+          end
+          pigeon_list[name][attribute].push descriptor.to_s
+      end
+    end
+  end
+return pigeon_list
 end
