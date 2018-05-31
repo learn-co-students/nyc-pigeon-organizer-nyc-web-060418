f108e9dae0aec7bfc5014954b09ac255352ac964
Changmin Lim <chang.lim92@gmail.com>
2018-05-31 06:10:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |label, classes|
+    classes.each do |character, names|
+      names.each do |name|
+        if result[name] == nil
+          result[name]= {}
+        end 
+        if result[name][label] == nil
+          result[name][label] = []
+          result[name][label].push(character.to_s) 
+        else
+          result[name][label].push(character.to_s)
+        end
+      end
+    end
+  end
+  result
 end
